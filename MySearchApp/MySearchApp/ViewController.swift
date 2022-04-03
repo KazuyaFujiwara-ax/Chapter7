da24d04c6158f45970c2191d81a2939219625e50
藤原和矢 <kazuya.fujiwara@axas-japan.co.jp>
2022-04-03 23:53:05 +0900

    商品検索画面の作成

---
+++ MySearchApp/MySearchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySearchApp
+//
+//  Created by AXLT0220-AP on 2022/04/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
