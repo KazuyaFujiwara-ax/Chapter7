bc1c16d933e9404dda36b2313899a479c52afe3e
藤原和矢 <kazuya.fujiwara@axas-japan.co.jp>
2022-04-10 23:22:03 +0900

    temp

---
+++ MySearchApp/MySearchApp/ItemTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ItemTableViewCell.swift
+//  MySearchApp
+//
+//  Created by AXLT0220-AP on 2022/04/10.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+    @IBOutlet weak var itemImageView: UIImageView!
+    //WIP
+}
